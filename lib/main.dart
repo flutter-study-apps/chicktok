efc3d89dc1e3ba92072bf4c1ab552d50ce4ec67a
ialexies <ialexies@gmail.com>
2021-09-09 00:28:16 +0800

    Working stream of products with data isloading check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
-import 'package:chicktok/widgets/app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
